fd4ee18658db894fd3c224aed65e56e1a2794fdc
sjmcg <sjmcg@DESKTOP-LCTGFBC>
2025-01-23 21:33:03 -0500

    Error handling

--- RandoTest/Program.cs
+++ RandoTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SuperMetroidReduxRandomizer.Settings;
 
 namespace SuperMetroidRandomizer
@@ -14,11 +15,44 @@ namespace SuperMetroidRandomizer
         [STAThread]
         static void Main(string[] args)
         {
+            
+            //// Convert Settings JSON into Settings Class
             string settingspath = args[0];
             JsonSerializer serializer = new JsonSerializer();
             StreamReader file = File.OpenText(settingspath);
             JsonTextReader json = new JsonTextReader(file);
-            Settings Settings = serializer.Deserialize<Settings>(json);
+
+            Settings Settings = null;
+            try
+            {
+                Settings = serializer.Deserialize<Settings>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("An error occurred: " + ex.Message);
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                Validation.ValidateSeed(Settings.seed);
+                Validation.ValidateFilePath(Settings.InputFile);
+                Validation.ValidateFilePath(Settings.OutputFile);
+                Validation.ValidateDifficulty(Settings.Difficulty);
+                Validation.ValidateItems(Settings.ItemsCount.Missiles, Settings.ItemsCount.SuperMissiles,Settings.ItemsCount.PowerBombs);
+                Validation.ValidatePositive(Settings.ItemsCount.Missiles);
+                Validation.ValidatePositive(Settings.ItemsCount.SuperMissiles);
+                Validation.ValidatePositive(Settings.ItemsCount.PowerBombs);
+                Validation.ValidatePositive(Settings.ItemsCount.EnergyTanks);
+                Validation.ValidatePositive(Settings.ItemsCount.ReserveTanks);
+                Validation.ValidateEnergy(Settings.ItemsCount.EnergyTanks);
+                Validation.ValidateReserve(Settings.ItemsCount.ReserveTanks);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("An error occurred: " + ex.Message);
+                Environment.Exit(1);
+            }
 
             var MainFunction = new MainFunction();
             MainFunction.CreateRom(Settings);
---
+++ RandoTest/Settings/Validation.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace SuperMetroidReduxRandomizer.Settings
+{
+    public class Validation
+    {
+        public class ValidationException : Exception
+        {
+            public ValidationException(string message) : base(message) { }
+        }
+        public static void ValidateSeed(string value)
+        {
+            int parsedSeed;
+            if (!int.TryParse(value, out parsedSeed) && value != "")
+            {
+                throw new ValidationException($"Seed must be an integer.");
+            }
+        }
+
+        public static void ValidateFilePath(string value)
+        {
+            try 
+            {
+               byte[] RomImage = File.ReadAllBytes(value);
+            }
+            catch 
+            {
+                throw new ValidationException($"{value} is not a valid file path.");
+            }
+        }
+
+        public static void ValidateDifficulty(string value)
+        {
+            if(value != "Casual" && value != "Speedrunner" && value != "Masochist")
+            {
+                throw new ValidationException($"Difficulty must be either 'Casual', 'Speedrunner', or 'Masochist'.");
+            }
+        }
+        public static void ValidateItems(int missiles, int bombs, int supers)
+        {
+            if (missiles + bombs + supers > 330)
+            {
+                throw new ValidationException($"Total of Missiles, Super Missiles, and Power Bombs should not exceed 330.");
+            }
+
+            if(!((missiles + bombs + supers) % 5 == 0))
+            {
+                throw new ValidationException($"Missiles, Super Missiles, and Power Bombs must be divisble by 5.");
+            }
+        }
+
+        public static void ValidatePositive(int value)
+        {
+            if (value < 0)
+            {
+                throw new ValidationException($"Item counts cannot be negative.");
+            }
+        }
+
+        public static void ValidateEnergy(int value)
+        {
+            if (value > 14)
+            {
+                throw new ValidationException($"Cannot have more than 14 energy tanks.");
+            }
+        }
+
+        public static void ValidateReserve(int value)
+        {
+            if (value > 4)
+            {
+                throw new ValidationException($"Cannot have more than 4 reserve tanks.");
+            }
+        }
+
+    }
+}
